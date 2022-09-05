3a218c63f836ddaf588bd5f03fe3fab160a904a4
Nurul-Pj <fajriantynurul99@gmail.com>
2022-09-05 18:56:49 +0700

    Post Test 28

---
+++ src/test/java/com/juaracoding/pageobject/dayduadelapan/Segitiga.java
@@ -0,0 +1,29 @@
+package com.juaracoding.pageobject.dayduadelapan;
+
+public class Segitiga {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count = 5;
+		for (int i = count; i >= 1; i--) {
+		  int temp = 0;
+		  int j = count - i + 1;
+		  while (temp != i) {
+		    if (j % 2 != 0) {
+		      if (!(i % 2 == 0 && temp >= i - 1)) {
+		    	  System.out.print(j);
+		      }
+		      temp++;
+		    } else {
+		    	System.out.print("_");
+		    }
+		    j++;
+		  }
+		  System.out.println("");
+		}
+
+
+	}
+
+}
